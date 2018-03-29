cd00567351582468c6cb22a952730337e20a0844
Владимир Иванов <ivanov-vi@gollard.ru>
2018-03-29 10:45:40 +0300

    fix: #14 #13

--- install.py
+++ install.py
@@ -56,9 +56,9 @@ class Loader_Xmlsoccer():
                 
                     if match['Time'] == 'Finished':
                         if row.finished == False:
+                            row.finished = True
                             closed = row.closeMatch()
                             if closed:
-                                row.finished = True
                                 row.save()
                 except KeyError:
                     pass
